de2187fc3a6d426a060c4daeff8e22006a4663de
SKR276 <sreeponnu06@gmail.com>
2024-02-09 09:07:00 +0530

    covid

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(Mainpage());
+}
+class Mainpage extends StatefulWidget {
+  const Mainpage({super.key});
+
+  @override
+  State<Mainpage> createState() => _MainpageState();
+}
+
+class _MainpageState extends State<Mainpage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
+
